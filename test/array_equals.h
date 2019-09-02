8acf975103a5aa1e423e1c0b03d29355326b91ca
eddyerburgh <edward.yerburgh@gmail.com>
2019-09-02 17:59:31 +0100

    [Fix] Use correct bounds for array_equals

--- test/array_equals.h
+++ test/array_equals.h
@@ -1,9 +1,10 @@
 #include <stdbool.h>
 
 bool array_equals(int a[], int b[], int n) {
-  int i;
-  for (i = 1; i <= n; i++) {
-    if (a[i] != b[i]) return false;
+  for (int i = 0; i < n; i++) {
+    if (a[i] != b[i]) {
+      return false;
+    }
   }
   return true;
 }
